a99387e9b8714c7c75e0c22cddd649dff7008219
sanjeevan2001 <2001sanjeevan@gmail.com>
2025-09-20 12:11:15 +0530

    changes

--- CarRental/Controllers/AdminDashboardController.cs
+++ CarRental/Controllers/AdminDashboardController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarRental.Data;
+using CarRental.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarRental.Controllers
 {
     public class AdminDashboardController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public AdminDashboardController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+           
+
             return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            var model = new AdminDashboardViewModel
+            {
+                TotalCars = _context.Cars.Count(),
+                AvailableCars = _context.Cars.Count(c => c.IsAvailable),
+                TotalBookings = _context.Bookings.Count(),
+                TotalCustomers = _context.Customers.Count()
+            };
+
+            return View(model);
         }
     }
 }
---
+++ CarRental/ViewModels/AdminDashboardViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CarRental.ViewModels
+{
+    public class AdminDashboardViewModel
+    {
+        public int TotalCars { get; set; }
+        public int AvailableCars { get; set; }
+        public int TotalBookings { get; set; }
+        public int TotalCustomers { get; set; }
+    }
+}
